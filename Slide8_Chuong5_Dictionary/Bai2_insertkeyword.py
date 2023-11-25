c376dbc8c721992e2efc85fdf2bc1eddef1c5c36
Gosu3 <thonguyenptit986@gmail.com>
2023-11-25 08:32:14 +0700

    Add Exam insertkey

--- Slide8_Chuong5_Dictionary/Bai2_insertkeyword.py
+++ Slide8_Chuong5_Dictionary/Bai2_insertkeyword.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 24 17:17:50 2023
+inventory = {
+        'gold': 500    
+    
+}
 
-@author: Admin
-"""
+new_key = 'key4'
+new_value = 'value4'
 
+inventory[new_key]= new_value
+
+
+print(inventory)
+
+
+#khai báo 1 từ điển mới gồm giá trị key:value
+#sử dụng 
+# my_dict.update(new_items)
+
+
+#sắp xếp item của key trong từ điển
+
+target_key = 'value3'
+
+sort_values = sorted(inventory[target_key])
+
+
+sort_dict = {target_key:sort_values, **{key:value for key,value in inventory.items() if key != target_key}}
+print(sort_dict)
