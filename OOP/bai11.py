eb79c0ec2619e4949f87650c79024f21b49494e4
Thọ Nguyễn <thonguyenptit986@gmail.com>
2023-12-09 09:05:20 +0700

    Chage

--- OOP/bai11.py
+++ OOP/bai11.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  2 14:31:43 2023
+class Vecto:
+    def __init__(self,hoanh_do_dau, tung_do_dau, hoanh_do_cuoi, tung_do_cuoi):
+        self.__hoanh_do_dau = hoanh_do_dau
+        self.__tung_do_dau = tung_do_dau
+        self.__hoanh_do_cuoi = hoanh_do_cuoi
+        self.__tung_do_cuoi = tung_do_cuoi
+    
+    
+    def kiem_tra_bang_nhau(self):
+        pass
+    def module_vecto(self):
+        pass
+    def Add_vecto(self):
+        pass
+    def Sub_vecto(self):
+        pass
+    def Tich_vecto(self):
+        pass
+    def kiem_tra_cung_phuong (self):
+        pass
 
-@author: thong
-"""
 
+vecto1 = Vecto(1, 2, 3, 4)
+vecto2 = Vecto(2, 4, 5, 1)
